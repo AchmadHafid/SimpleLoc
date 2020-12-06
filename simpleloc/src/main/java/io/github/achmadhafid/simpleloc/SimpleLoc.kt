75554d615b1a9073d6029432b9eae4c29bcfee5e
hafid <ach.hafid@gmail.com>
2020-12-06 13:26:11 +0700

    add some todo for next development

--- simpleloc/src/main/java/io/github/achmadhafid/simpleloc/SimpleLoc.kt
+++ simpleloc/src/main/java/io/github/achmadhafid/simpleloc/SimpleLoc.kt
@@ -8,6 +8,13 @@ internal const val CHECK_LOCATION_SETTING_RESULT_DELAY = 250L
 
 internal object LocationServiceRepairError : Exception()
 
+// TODO 06/12/2020 handle permission request scheme while in-use
+// 1. give client option whether asking foreground only or all the time
+// 2. when request all the time, add callback when user only select while in-user and add helper flow to handle it
+// TODO 06/12/2020 support offline location tracking
+// TODO 06/12/2020 update documentation
+// TODO 06/12/2020 update sample app
+
 // TODO 06/12/2020 when targeting API 30:
 // 1. Create separate location permission request for FOREGROUND & BACKGROUND location access
 //    https://developer.android.com/training/location/permissions#request-location-access-runtime
