1e3764623c66d41679407d870c77d5cf9245d675
hafid <ach.hafid@gmail.com>
2020-12-12 18:51:47 +0700

    code clean-up

--- simpleloc/src/main/java/io/github/achmadhafid/simpleloc/SimpleLoc.kt
+++ simpleloc/src/main/java/io/github/achmadhafid/simpleloc/SimpleLoc.kt
@@ -8,11 +8,6 @@ internal const val CHECK_LOCATION_SETTING_RESULT_DELAY = 250L
 
 internal object LocationServiceRepairError : Exception()
 
-// TODO 06/12/2020 update documentation
-// TODO 06/12/2020 update sample app
-
 // TODO 06/12/2020 when targeting API 30:
-// 1. Create separate location permission request for FOREGROUND & BACKGROUND location access
-//    https://developer.android.com/training/location/permissions#request-location-access-runtime
 // 2. Create education dialog before asking BACKGROUND location access
 //    https://developer.android.com/training/location/permissions#request-background-location
